9b14e23b2b6da32518688eaef1de28ad093ab313
willem <willem@leit.so>
2020-06-25 13:16:38 +0200

    Add sources

---
+++ src/main/kotlin/dev/wnuke/nukeClient/NukeClient.kt
@@ -0,0 +1,12 @@
+package dev.wnuke.nukeClient
+
+import net.fabricmc.api.ModInitializer
+
+class NukeClient : ModInitializer {
+
+    override fun onInitialize() {
+        println("**************************")
+        println("Hello from NukeClient")
+        println("**************************")
+    }
+}
